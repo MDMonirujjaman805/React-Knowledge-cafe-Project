51f609d160dc93523d2f8d6516f1ddb336beecfa
MD Monirujjaman <mdmonirujjaman@MDs-MacBook-Air.local>
2025-07-14 08:02:50 +0600

    Project setup done.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='bg-red-400 text-3xl rounded-lg '>React Knowledge cafe Project</h1>
+      
+    </>
+  )
+}
+
+export default App
