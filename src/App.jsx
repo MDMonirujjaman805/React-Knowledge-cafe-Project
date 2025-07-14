bfca21328d35b2b5b759ba86329fc8a69754d7d6
MD Monirujjaman <mdmonirujjaman@MDs-MacBook-Air.local>
2025-07-14 13:32:10 +0600

    header section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css'
+import "./App.css";
+import Header from "./Componentens/Header/Header";
+import Blogs from "./Componentens/Blogs/Blogs";
+import Bookmarks from "./Componentens/Bookmarks/Bookmarks";
 
 function App() {
-
   return (
     <>
-      
-      <h1 className='bg-red-400 text-3xl rounded-lg '>React Knowledge cafe Project</h1>
-      
+      <Header></Header>
+      <div className="md:flex max-w-11/12 mx-auto">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Componentens/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Profile from "../../assets/images/profile.png"
+const Header = () => {
+  return (
+    <div className="max-w-11/12 mx-auto flex justify-between items-center p-2 border-b-1">
+      <h1 className="text-4xl "> Knowledge Cafe</h1>
+      <img src={Profile} alt="Profile-Image" />
+    </div>
+  );
+};
+
+export default Header;
